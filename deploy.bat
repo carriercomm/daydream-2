f6dd61a3db29838b801ef5f4e8393a1ce123a6e8
sippeangelo <holmberg.simon@gmail.com>
2014-09-14 02:38:36 +0200

    Deploy script updated

---
+++ deploy.bat
@@ -0,0 +1,25 @@
+@ECHO off
+
+CALL :deploy Debug
+CALL :deploy Release
+GOTO:eof
+
+:deploy
+SET Configuration=%~1
+SET ConfigPath=bin\%~1
+ECHO Deploying %Configuration%
+:: Asset folders
+MKLINK "%ConfigPath%\Models\" "assets\Models" /J
+MKLINK "%ConfigPath%\Textures\" "assets\Textures\" /J
+MKLINK "%ConfigPath%\Sounds\" "assets\Sounds\" /J
+:: Shaders
+MKLINK "%ConfigPath%\Shaders\" "src\dd\Core\Shaders\" /J
+:: DLLs
+COPY "libs\glfw-3.0.4\lib\%Configuration%\glfw3.dll" "%ConfigPath%\glfw3.dll"
+IF %~1==Debug (
+	COPY "libs\glew-1.11.0\bin\%Configuration%\Win32\glew32d.dll" "%ConfigPath%\glew32d.dll"
+)
+IF %~1==Release (
+	COPY "libs\glew-1.11.0\bin\%Configuration%\Win32\glew32.dll" "%ConfigPath%\glew32.dll"
+)
+GOTO:eof
