4f2a04c3d6ae9e8eb0b11d209f58c9488b0e8d15
sippeangelo <holmberg.simon@gmail.com>
2015-04-27 05:51:38 +0200

    Fixed Windows deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,18 +1,18 @@
-@ECHO off
+@ECHO on
 
-SET DeployLocation=bin/
+SET DeployLocation=bin\
 
-ECHO Deploying resources to %Configuration%
+ECHO Deploying resources to %DeployLocation%
 :: Asset folders
 MKLINK "%DeployLocation%\Models\" "assets\Models" /J
 MKLINK "%DeployLocation%\Textures\" "assets\Textures\" /J
 MKLINK "%DeployLocation%\Sounds\" "assets\Sounds\" /J
 :: Shaders
-MKLINK "%ConfigPath%\Shaders\" "src\dd\Core\Shaders\" /J
+MKLINK "%DeployLocation%\Shaders\" "src\dd\Core\Shaders\" /J
 :: Platform specific binaries
 IF "%~1"=="" GOTO :EOF
 ECHO Deploying %1 binaries to %DeployLocation%
-COPY "deps/bin/%1/x64/*.dll" "%DeployLocation%"
+COPY "deps\bin\%1\x64\*.dll" "%DeployLocation%"
 :: Licenses
 ::COPY "libs\assimp-3.1.1\LICENSE" "%ConfigPath%\Assimp License.txt"
 ::COPY "libs\glew-1.11.0\LICENSE.txt" "%ConfigPath%\GLEW License.txt"
